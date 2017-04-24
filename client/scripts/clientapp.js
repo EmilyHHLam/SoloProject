a7241195291dc42a7533f0b9fa7df7bbb9559e62
EmilyH <EmilyHHLam@gmail.com>
2017-04-24 15:55:25 -0500

    setup the nav features

---
+++ client/scripts/clientapp.js
@@ -0,0 +1,83 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+/// Routes ///
+myApp.config(['$routeProvider', '$locationProvider',
+      function($routeProvider, $locationProvider) {
+  $locationProvider.hashPrefix('');
+
+  $routeProvider
+    .when('/home', {
+      templateUrl: '/views/templates/home.html',
+      controller: 'LoginController',
+    })
+    .when('/register', {
+      templateUrl: '/views/templates/register.html',
+      controller: 'LoginController'
+    })
+    .when('/user', {
+      templateUrl: '/views/templates/user.html',
+      controller: 'UserController',
+      resolve: {
+        getuser : ['UserService', function(UserService){
+          return UserService.getuser();
+        }]
+      }
+    })
+    .when('/info', {
+      templateUrl: '/views/templates/info.html',
+      controller: 'InfoController',
+      resolve: {
+        getuser : ['UserService', function(UserService){
+          return UserService.getuser();
+        }]
+      }
+    })
+    .when('/events', {
+      templateUrl: '/views/templates/events.html',
+      controller: 'InfoController',
+      resolve: {
+        getuser : ['UserService', function(UserService){
+          return UserService.getuser();
+        }]
+      }
+    })
+    .when('/milestones', {
+      templateUrl: '/views/templates/milestones.html',
+      controller: 'InfoController',
+      resolve: {
+        getuser : ['UserService', function(UserService){
+          return UserService.getuser();
+        }]
+      }
+    })
+    .when('/medicalinfo', {
+      templateUrl: '/views/templates/medicalinfo.html',
+      controller: 'InfoController',
+      resolve: {
+        getuser : ['UserService', function(UserService){
+          return UserService.getuser();
+        }]
+      }
+    })
+    .when('/resources', {
+      templateUrl: '/views/templates/resources.html',
+      controller: 'InfoController',
+      resolve: {
+        getuser : ['UserService', function(UserService){
+          return UserService.getuser();
+        }]
+      }
+    })
+    .when('/contact', {
+      templateUrl: '/views/templates/contact.html',
+      controller: 'InfoController',
+      resolve: {
+        getuser : ['UserService', function(UserService){
+          return UserService.getuser();
+        }]
+      }
+    })
+    .otherwise({
+      redirectTo: 'home'
+    });
+}]);
