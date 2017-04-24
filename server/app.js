c68bb919fea117660d4982af1b2356dd80e0d46d
EmilyH <EmilyHHLam@gmail.com>
2017-04-24 16:54:11 -0500

    setup the gad db

--- server/app.js
+++ server/app.js
@@ -46,7 +46,8 @@ if(process.env.MONGODB_URI != undefined) {
     mongoURI = process.env.MONGODB_URI;
 } else {
     // use the local database server
-    mongoURI = 'mongodb://localhost:27017/passport';
+    // mongoURI = 'mongodb://localhost:27017/passport';
+    mongoURI = 'mongodb://localhost:27017/gad';
 }
 
 // var mongoURI = "mongodb://localhost:27017/passport";
