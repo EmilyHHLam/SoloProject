572cebf643094958b165bc06fe6a38c7ce6ef406
EmilyH <EmilyHHLam@gmail.com>
2017-05-05 14:33:25 -0500

    edit the connection

--- server/app.js
+++ server/app.js
@@ -50,15 +50,15 @@ app.use('/', index);
 var mongoURI = '';
 // process.env.MONGODB_URI will only be defined if you
 // are running on Heroku
-// if(process.env.MONGODB_URI != undefined) {
+ if(process.env.MONGODB_URI != undefined) {
 //     // use the string value of the environment variable
-//     mongoURI = process.env.MONGODB_URI;
-// } else {
+     mongoURI = process.env.MONGODB_URI;
+ } else {
     // use the local database server
     // mongoURI = 'mongodb://localhost:27017/passport';
-    //mongoURI = 'mongodb://localhost:27017/gad';
-    mongoURI = "mongodb://users:1234567@ds147480.mlab.com:47480/emily-testdb";
-//}
+    mongoURI = 'mongodb://localhost:27017/gad';
+    //mongoURI = "mongodb://users:1234567@ds147480.mlab.com:47480/emily-testdb";
+}
 
 // var mongoURI = "mongodb://localhost:27017/passport";
 var mongoDB = mongoose.connect(mongoURI).connection;
